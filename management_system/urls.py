2fbe8122892e7fd171d4f7f1b7732747a2d5505f
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2019-12-13 17:13:12 +0900

    [make] formの作成
    formの作成
    データベースの再設計のため一旦離脱
    （実績時間の必須を解除）

--- management_system/urls.py
+++ management_system/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from .views import RequestAddView
 
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:request_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:request_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:request_id>/vote/', views.vote, name='vote'),
+
+    path('add/', RequestAddView.as_view(),name='add'), 
 ]
--- management_system/views.py
+++ management_system/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
 
+from django.views.generic.edit import CreateView 
+from .forms import RequestForm
+from .models import Request
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return HttpResponse("Hello, world. You're at the management_system index.")
+
+def detail(request, request_id):
+    return HttpResponse("You're looking at request %s." % request_id)
+
+def results(request, request_id):
+    response = "You're looking at the results of request %s."
+    return HttpResponse(response % request_id)
+
+def vote(request, request_id):
+    return HttpResponse("You're voting on request %s." % request_id)
+
+class RequestAddView(CreateView):
+    model = Request
+    fields = ('scheduled_entry_datetime', 'scheduled_exit_datetime', 'purpose_admission', 'email')
+    template_name = 'management_system/request_add.html'
+    success_url = '/'
+
+
