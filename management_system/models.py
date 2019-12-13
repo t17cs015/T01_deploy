99ea9aa56ff9a6267cbf4d4ef618e0707543da1b
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2019-12-13 13:45:50 +0900

    [make] models.pyにデータベースの型を追加
    models.pyにデータベースの型を追加
    url.pyを編集してアクセスできるように変更
    adminによりデータベースを操作できるように変更

---
+++ management_system/models.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Customer(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=60)
+    organization_name = models.CharField(max_length=60)
+    tell_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Request(models.Model):
+    scheduled_entry_datetime = models.DateTimeField('date published')
+    scheduled_exit_datetime = models.DateTimeField('date published')
+    entry_datetime = models.DateTimeField('date published')
+    exit_datetime = models.DateTimeField('date published')
+    purpose_admission = models.CharField(max_length=255)
+    request_datetime = models.DateTimeField('date published')
+    email = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    # 通し番号はID
+    approval = models.IntegerField(default=0)
+    password = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.id)
--- team/urls.py
+++ team/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # @UnresolvedImport
+from django.urls import path, include # @UnresolvedImport
+import management_system.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('management_system/', include('management_system.urls'), name='management_system'),
 ]
