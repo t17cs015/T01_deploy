38c18b028c32a5665a3c7d1c5dcd8c80cafcbd52
kohei nishimuta <oronamincpp@gmail.com>
2019-12-16 15:39:00 +0900

    [add] --help option

--- run.sh
+++ run.sh
@@ -10,7 +10,18 @@ elif [[ $1 == "clean" ]]; then
 elif [[ $1 == "all" ]]; then
     $0 clean
     $0 mig
+elif [[ $1 == "--help" ]]; then
+    echo "使用法 : ./run.sh [オプション]..."
+    echo "オプションをつけずに実行すると python3.5 manage.py runserver が実行されます。"
+    echo ""
+    echo "オプション"
+    echo "  mig                 makemigrations と migrate を同時に実行する"
+    echo "  clean               db.sqlit3 と migrations ディレクトリ以下のファイルを削除する"
+    echo "  all                 clean オプションを付けて実行した後に"
+    echo "                      mig オプションを付けて実行したのと同じ結果を与える"
+    echo "  その他オプション    python3.5 manage.py に与えられた引数をそのまま渡して実行する"
 else
     python3.5 manage.py $@
 fi
+
 
