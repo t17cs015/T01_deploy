b09a37e86700191ce858720b286725436e93054e
t17cs015 <t17cs015@gmail.com>
2020-01-18 01:20:43 +0900

    [fix] 入力画面のボックス位置の修正
    外部設計書に基づき入力フォームの位置を変更

--- management_system/forms.py
+++ management_system/forms.py
@@ -27,12 +27,12 @@ class RequestGetForm(forms.ModelForm):
         fields = ['scheduled_entry_datetime', 'scheduled_exit_datetime','entry_datetime','exit_datetime', 'purpose_admission',  'email']
 
 class RequestSendForm(forms.Form):
-    name = forms.CharField(label = '名前')
     organization_name = forms.CharField(label = '企業名',max_length=60)
+    name = forms.CharField(label = '名前')
+    tell_number_validator = RegexValidator(regex=r'^(\+([0-9]){1,4}-[1-9]([0-9]){1,3}|0([0-9]){1,3})-([0-9]){1,4}-([0-9]){1,4}$', message=("半角数字とハイフンを用いて電話番号を入力してください"))
+    tell_number = forms.CharField(max_length = 19, label = '電話番号',validators=[tell_number_validator])
     email = forms.EmailField(label = 'email')
     scheduled_entry_datetime = forms.DateTimeField(label = '入館時間')
     scheduled_exit_datetime = forms.DateTimeField(label = '退館時間')
     purpose_admission = forms.CharField(label = '申請理由',max_length=255)
-    tell_number_validator = RegexValidator(regex=r'^(\+([0-9]){1,4}-[1-9]([0-9]){1,3}|0([0-9]){1,3})-([0-9]){1,4}-([0-9]){1,4}$', message=("半角数字とハイフンを用いて電話番号を入力してください"))
-    tell_number = forms.CharField(max_length = 19, label = '電話番号',validators=[tell_number_validator])
 
