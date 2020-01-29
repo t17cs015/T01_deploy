e9a3128eb0c9eac0f4ab89589fe27f96d29c069b
t17cs015 <t17cs015@dw11.kki.yamanashi.ac.jp>
2020-01-29 17:31:00 +0900

    [fix] urls.pyのコメントアウトを削除
    上記の通りです

--- management_system/urls.py
+++ management_system/urls.py
@@ -6,12 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', RequestMainView.as_view(), name='main'),
-    # ex: /polls/5/
-    # path('<int:request_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    # path('<int:request_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    # path('<int:request_id>/vote/', views.vote, name='vote'),
     path('add/', RequestAddView.as_view(),name='add'), 
     path('login/',RequestLoginView.as_view(),name='login'),
     path('performance/<int:pk>/', RequestPerformanceView.as_view(),name='performance'), 
