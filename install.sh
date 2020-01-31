577d8d133519d0b778ff73f7e2b303987ad753da
kohei nishimuta <oronamincpp@gmail.com>
2020-01-31 14:33:17 +0900

    [add] インストール用のスクリプトを作成
    マイグレーション、管理者ユーザの作成と削除を実装

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 if [[ $# == 0 ]]; then
-    $0 runserver
+    $0 clean
+    $0 mig
+    (
+        echo "admin" && \
+        echo "p@ssw0rd"
+    ) | $0 adduser
 elif [[ $1 == "mig" ]]; then
     $0 makemigrations management_system && $0 migrate
 elif [[ $1 == "clean" ]]; then
@@ -11,21 +16,19 @@ elif [[ $1 == "adduser" ]]; then
     MAIL="admin@myproject.com"
     read -p "Admistrator Name: " USER
     read -sp "Admistrator Password: " PASS
-    echo $USER
-    echo $PASS
     (
         echo "from django.contrib.auth import get_user_model" && \
         echo "User = get_user_model()" && \
         echo "User.objects.create_superuser('"$USER"', '"$MAIL"', '"$PASS"')" \
     ) | $0 shell
-elif [[ $1 == "install" ]]; then
-    $0 clean
-    $0 mig
+elif [[ $1 == "rmuser" ]]; then
+    read -p "Admistrator Name: " USER
     (
-        echo "admin" && \
-        echo "password"
-    ) | $0 adduser
-    $0
+        echo "from django.contrib.auth import get_user_model" && \
+        echo "User = get_user_model()" && \
+        echo "print(User.objects.filter(is_superuser=True).filter(username='"$USER"'))" && \
+        echo "User.objects.filter(is_superuser=True).filter(username='"$USER"').delete()"
+    ) | $0 shell
 elif [[ $1 == "--help" ]]; then
     echo "使用法 : ./run.sh [オプション]..."
     echo "オプションをつけずに実行すると python3.5 manage.py runserver が実行されます。"
@@ -34,9 +37,7 @@ elif [[ $1 == "--help" ]]; then
     echo "  mig                 makemigrations と migrate を同時に実行する"
     echo "  clean               db.sqlit3 と migrations ディレクトリ以下のファイルを削除する"
     echo "  adduser             管理者ユーザーを追加する"
-    echo "  dbinit              データベースにテスト用データを追加する"
-    echo "  all                 強制再マイグレーションを実行し、管理者ユーザーと"
-    echo "                      テスト用データをデータベースに追加しサーバーを実行する"
+    echo "  rmuser              管理者ユーザーを削除する"
     echo "  その他オプション    python3.5 manage.py に与えられた引数をそのまま渡して実行する"
 else
     python3.5 manage.py $@
