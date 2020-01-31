4607fd60197a50c5806d55e2c6a9d99046ea46b1
t17cs015 <t17cs015@lw11.kki.yamanashi.ac.jp>
2020-01-31 17:09:18 +0900

    [fix] staticROOTの削除

--- team/settings.py
+++ team/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hb(+4sj#04yp4)1hyatfk$_dz+6!m-^lgexm17c2xx1pszh$$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
 
 
 LOGIN_REDIRECT_URL = 'admin_list'
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # mail
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
